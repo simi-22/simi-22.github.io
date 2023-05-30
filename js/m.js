4393aa213f5ba13e02cfe47bcf6ffb591f2b3fd6
simi-22 <ssimii0222@gmail.com>
2023-05-30 15:06:14 +0900

    tointrobutton

--- js/m.js
+++ js/m.js
@@ -15,5 +15,30 @@ let typingPrint = setInterval(function(){
             saying.innerHTML = '';
             count = 0;
         }
-    },150)//내가 만들었지만 이해안감...
+    },100)//내가 만들었지만 이해안감...
 
+
+
+const welcomeBox = document.querySelector('#welcome-box');
+const intro = document.querySelector('#intro');
+
+
+const welcomeMove = () => {
+    welcomeBox.classList.add('transparency')
+    setTimeout(function(){
+        welcomeBox.classList.add('hidden')
+        intro.classList.add('active')
+    },2000)
+}
+
+welcomeBox.addEventListener('click',function(){setTimeout(welcomeMove,500)})
+
+
+const nextPageButton = document.querySelector('.n-btn-wrap')
+console.log(nextPageButton)
+const btnActive = () => {
+    setTimeout(function(){
+        nextPageButton.classList.add('active')
+    },3000)
+}
+btnActive()
